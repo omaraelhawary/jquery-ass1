b1f82de12b3cbb6908e7fbe81d4f31bd3ca8703b
Omar ElHawary <omaraelhawary@gmail.com>
2020-05-08 19:11:52 +0200

    Duration Section

--- js/app.js
+++ js/app.js
@@ -18,3 +18,35 @@ $(document).ready(function()
          $(".parg").not($(this).next()).slideUp(500);     
     });              
 });
+
+/* counter */
+function countdown(){
+    var today = new Date();
+    var eventDate = new Date(2020, 12, 17);
+    var currentTime = today.getTime();
+    var eventTime = eventDate.getTime();
+    var remainTime = eventTime - currentTime;
+    var s = Math.floor(remainTime / 1000);
+    var m = Math.floor(s / 60);
+    var h = Math.floor(m / 60);
+    var d = Math.floor(h / 24)-30;
+
+    h %= 24;
+    m %= 60;
+    s %= 60;
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    document.getElementById("days").innerHTML = "<h3>"+  d + " D"+"</h3>";
+    document.getElementById("days").innerHTML = "<h3>"+  d + " D"+"</h3>";
+
+    document.getElementById("hours").innerHTML ="<h3>"+  h + " h"+"</h3>";
+    document.getElementById("minutes").innerHTML = "<h3>"+  m + " m"+"</h3>";
+    document.getElementById("seconds").innerHTML = "<h3>"+  s + " s"+"</h3>";
+
+    setTimeout(countdown, 1000);
+}
+
+countdown();
