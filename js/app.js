b2238ff3f2e78143eb793e975b17194646a843b4
Omar ElHawary <omaraelhawary@gmail.com>
2020-05-08 17:50:21 +0200

    Details Section

--- js/app.js
+++ js/app.js
@@ -8,3 +8,13 @@ function closeMenu() {
     $("#menu").css("width","0%");
     $(".main").css("marginLeft","0%");
 }
+
+/* collapse */
+$(document).ready(function()
+{                
+      $("#detials .parg:first").css("display","block");
+    $("#details h3").click(function(){      
+        $(this).next().slideToggle(500);
+         $(".parg").not($(this).next()).slideUp(500);     
+    });              
+});
