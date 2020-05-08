a85c07d52888c632f1f62d67c12c519fc752a051
Omar ElHawary <omaraelhawary@gmail.com>
2020-05-08 17:47:03 +0200

    Home section

---
+++ js/app.js
@@ -0,0 +1,10 @@
+/* Menu buttons */
+function openMenu() {
+    $("#menu").css("width","25%");
+    $(".main").css("marginLeft","25%");
+}
+
+function closeMenu() {
+    $("#menu").css("width","0%");
+    $(".main").css("marginLeft","0%");
+}
